41e17fc797a6a603cf8beafb327eeef657b168f7
enzo-gavini <enzo.gavini@laplateforme.io>
2026-05-28 15:04:29 +0200

    feat(persistence-service): add Chapter entity

---
+++ persistence-service/src/main/java/com/enzo/persistenceservice/entity/Chapter.java
@@ -0,0 +1,25 @@
+package com.enzo.persistenceservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chapter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+    private String description;
+    private int orderIndex;
+}
