d0fc7420b26b520a66e9193994d287e50cfdb04b
enzo-gavini <enzo.gavini@laplateforme.io>
2026-05-29 09:06:27 +0200

    feat(persitence-service): add UserService

---
+++ persistence-service/src/main/java/com/enzo/persistenceservice/service/UserService.java
@@ -0,0 +1,40 @@
+package com.enzo.persistenceservice.service;
+
+import com.enzo.persistenceservice.entity.User;
+import com.enzo.persistenceservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found by email"));
+    }
+
+    public User updateProfile(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+
+}
