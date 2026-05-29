686932f6bfdaa8905782f8870734a595b995102f
enzo-gavini <enzo.gavini@laplateforme.io>
2026-05-29 09:42:43 +0200

    feat(persistence-service): add ChapterService

---
+++ persistence-service/src/main/java/com/enzo/persistenceservice/service/ChapterService.java
@@ -0,0 +1,32 @@
+package com.enzo.persistenceservice.service;
+
+import com.enzo.persistenceservice.entity.Chapter;
+import com.enzo.persistenceservice.repository.ChapterRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ChapterService {
+    private final  ChapterRepository chapterRepository;
+
+    public Chapter create(Chapter chapter) {
+        return chapterRepository.save(chapter);
+    }
+
+    public List<Chapter> findAll() {
+        return chapterRepository.findAll();
+    }
+
+    public Chapter findById(Long id){
+        return chapterRepository.findById(id).orElseThrow(() -> new RuntimeException("Chapter not found"));
+    }
+
+    public Chapter updateChapter(Chapter chapter) {
+        return chapterRepository.save(chapter);
+    }
+
+    public void delete(Long id) {chapterRepository.deleteById(id);}
+}
