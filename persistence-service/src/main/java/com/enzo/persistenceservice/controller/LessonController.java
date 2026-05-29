fd76f46f9835c4133fd39affdb1b4868ee9d1257
enzo-gavini <enzo.gavini@laplateforme.io>
2026-05-29 21:17:56 +0200

    feat(persistence-service): add LessonController with REST endpoints

---
+++ persistence-service/src/main/java/com/enzo/persistenceservice/controller/LessonController.java
@@ -0,0 +1,40 @@
+package com.enzo.persistenceservice.controller;
+
+import com.enzo.persistenceservice.entity.Lesson;
+import com.enzo.persistenceservice.service.LessonService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/lessons")
+@AllArgsConstructor
+public class LessonController {
+    private final LessonService lessonService;
+
+    @PostMapping
+    public Lesson save(@RequestBody Lesson lesson) {
+        return lessonService.create(lesson);
+    }
+
+    @GetMapping
+    public List<Lesson> getAllLesson() {
+        return lessonService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Lesson getLessonById(@PathVariable Long id) {
+        return lessonService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Lesson updateLesson(@PathVariable Long id, @RequestBody Lesson lesson) {
+        return lessonService.updateLesson(lesson);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLesson(@PathVariable Long id) {
+        lessonService.delete(id);
+    }
+}
