3b708ffeb2338dd833bd8258e3333da2fa529002
enzo-gavini <enzo.gavini@laplateforme.io>
2026-05-29 09:21:47 +0200

    feat(persistence-service): add existence checks to UserService

--- persistence-service/src/main/java/com/enzo/persistenceservice/service/UserService.java
+++ persistence-service/src/main/java/com/enzo/persistenceservice/service/UserService.java
@@ -36,5 +36,12 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
 
 }
