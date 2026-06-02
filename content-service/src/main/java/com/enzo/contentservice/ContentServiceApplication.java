f56ed672e6ba64180ea3337a9907e8a04d02a6da
enzo-gavini <enzo.gavini@laplateforme.io>
2026-06-02 20:34:51 +0200

    chore(content-service): add application.yaml and enable Feign/Consul

--- content-service/src/main/java/com/enzo/contentservice/ContentServiceApplication.java
+++ content-service/src/main/java/com/enzo/contentservice/ContentServiceApplication.java
@@ -2,8 +2,12 @@ package com.enzo.contentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ContentServiceApplication {
 
     public static void main(String[] args) {
