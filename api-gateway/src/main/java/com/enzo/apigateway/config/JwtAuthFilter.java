fb2a8894c13efae307292c03032b92fe6296ec52
enzo-gavini <enzo.gavini@laplateforme.io>
2026-06-10 10:04:34 +0200

    feat(api-gateway): add JwtService and JwtAuthFilter

---
+++ api-gateway/src/main/java/com/enzo/apigateway/config/JwtAuthFilter.java
@@ -0,0 +1,54 @@
+package com.enzo.apigateway.config;
+
+
+import com.enzo.apigateway.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter implements GlobalFilter, Ordered {
+    private final JwtService jwtService;
+
+    private final List<String> publicRoutes = List.of("/auth/register", "/auth/login");
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().toString();
+
+        if (isPublicRoute(path)) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        String token = authHeader.substring(7);
+
+        if (!jwtService.validateToken(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+private boolean isPublicRoute(String path) {
+        return publicRoutes.stream().anyMatch(path::startsWith);
+}
+
+@Override
+    public int getOrder() {
+        return -1;
+}
+}
---
+++ api-gateway/src/main/java/com/enzo/apigateway/service/JwtService.java
@@ -0,0 +1,45 @@
+package com.enzo.apigateway.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = extractAllClaims(token);
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
