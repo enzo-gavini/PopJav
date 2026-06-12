aca3ebfe20b31210cc7a531b62e7568c91fb4a88
enzo-gavini <enzo.gavini@laplateforme.io>
2026-06-12 14:06:18 +0200

    feat(ui-service): add QuizController with CRUD pages

---
+++ ui-service/src/main/java/com/enzo/uiservice/contoller/QuizController.java
@@ -0,0 +1,60 @@
+package com.enzo.uiservice.contoller;
+
+import com.enzo.uiservice.dto.QuizDTO;
+import com.enzo.uiservice.proxy.QuizFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/quizzes")
+@AllArgsConstructor
+public class QuizController {
+    private final QuizFeignClient quizFeignClient;
+
+    @GetMapping
+    public String showQuiz(Model model) {
+        List<QuizDTO> quizzes = quizFeignClient.getAllQuiz();
+        model.addAttribute("quizzes", quizzes);
+        return "quizzes";
+    }
+
+    @GetMapping("/{id}")
+    public String showQuizById(Model model, @PathVariable Long id) {
+        model.addAttribute("quizDTO", quizFeignClient.getQuizById(id));
+        return "quiz-detail";
+    }
+
+    @GetMapping("/create")
+    public String showCreatePage(Model model) {
+        model.addAttribute("quizDTO", new QuizDTO());
+        return "quiz-create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute ("quizDTO") QuizDTO quiz) {
+        quizFeignClient.save(quiz);
+        return "redirect:/quizzes";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditPage(@PathVariable Long id,  Model model) {
+        model.addAttribute("quizDTO", quizFeignClient.getQuizById(id));
+        return "quiz-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateQuiz(@PathVariable Long id, @ModelAttribute("quizDTO") QuizDTO quiz){
+        quizFeignClient.updateQuiz(id, quiz);
+        return "redirect:/quizzes";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteQuiz(@PathVariable Long id) {
+        quizFeignClient.deleteQuiz(id);
+        return "redirect:/quizzes";
+    }
+}
