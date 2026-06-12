b64e6394e54735fcd389717bc7f797b31b899b65
enzo-gavini <enzo.gavini@laplateforme.io>
2026-06-12 10:01:55 +0200

    feat(ui-service): add AuthController with login and register

---
+++ ui-service/src/main/java/com/enzo/uiservice/contoller/AuthController.java
@@ -0,0 +1,49 @@
+package com.enzo.uiservice.contoller;
+
+
+import com.enzo.uiservice.dto.AuthResponseDTO;
+import com.enzo.uiservice.dto.LoginRequestDTO;
+import com.enzo.uiservice.dto.RegisterRequestDTO;
+import com.enzo.uiservice.proxy.AuthFeignClient;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+    private final AuthFeignClient authFeignClient;
+
+    @GetMapping("/register")
+    public String showRegisterPage(Model model) {
+        model.addAttribute("registerRequest", new RegisterRequestDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute RegisterRequestDTO request, HttpSession session) {
+        AuthResponseDTO reponse = authFeignClient.register(request);
+        session.setAttribute("token", reponse.getToken());
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage (Model model) {
+        model.addAttribute("loginRequest", new LoginRequestDTO());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginRequestDTO request, HttpSession session) {
+        AuthResponseDTO response = authFeignClient.login(request);
+        session.setAttribute("token", response.getToken());
+        return "redirect:/";
+    }
+
+}
