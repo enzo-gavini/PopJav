140f871f89f39834e6e4bf93551932bff28349d2
enzo-gavini <enzo.gavini@laplateforme.io>
2026-06-12 14:18:40 +0200

    feat(ui-service): add QuestionController with CRUD pages

---
+++ ui-service/src/main/java/com/enzo/uiservice/contoller/QuestionController.java
@@ -0,0 +1,61 @@
+package com.enzo.uiservice.contoller;
+
+
+import com.enzo.uiservice.dto.QuestionDTO;
+import com.enzo.uiservice.proxy.QuestionFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/questions")
+@AllArgsConstructor
+public class QuestionController {
+    private final QuestionFeignClient questionFeignClient;
+
+    @GetMapping
+    public String showQuestions(Model model) {
+        List<QuestionDTO> questions = questionFeignClient.getAllQuestion();
+        model.addAttribute("questions", questions);
+        return "questions";
+    }
+
+    @GetMapping("/{id}")
+    public String showQuestionById(Model model, @PathVariable Long id) {
+        model.addAttribute("questionDTO", questionFeignClient.getQuestionById(id));
+        return "question-details";
+    }
+
+    @GetMapping("/create")
+    public String showCreatePage(Model model) {
+        model.addAttribute("questionDTO", new QuestionDTO());
+        return "question-create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute ("questionDTO") QuestionDTO question) {
+        questionFeignClient.save(question);
+        return "redirect:/questions";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditPage(@PathVariable Long id, Model model) {
+        model.addAttribute("questionDTO", questionFeignClient.getQuestionById(id));
+        return "question-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateQuestion(@PathVariable Long id, @ModelAttribute("questionDTO") QuestionDTO question) {
+        questionFeignClient.updateQuestion(id, question);
+        return "redirect:/questions";
+    }
+
+    @GetMapping("/delete/{id}")
+    public  String deleteQuestion(@PathVariable Long id) {
+        questionFeignClient.deleteQuestion(id);
+        return "redirect:/questions";
+    }
+}
