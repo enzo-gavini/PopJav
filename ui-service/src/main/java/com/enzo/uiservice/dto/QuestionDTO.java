464f04e3b67074d5f20f1a1f5d7a7a0916f66965
enzo-gavini <enzo.gavini@laplateforme.io>
2026-06-16 15:40:57 +0200

    feat(ui-service): add question templates and fix QuestionDTO with answers field

--- ui-service/src/main/java/com/enzo/uiservice/dto/QuestionDTO.java
+++ ui-service/src/main/java/com/enzo/uiservice/dto/QuestionDTO.java
@@ -2,9 +2,12 @@ package com.enzo.uiservice.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class QuestionDTO {
     private Long id;
     private String text;
     private Long quizId;
+    List<AnswerDTO> answers;
 }
