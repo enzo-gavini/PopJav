a75e58a25874ef77d51335ee7ef0c24ea5359862
enzo-gavini <enzo.gavini@laplateforme.io>
2026-06-23 13:25:39 +0200

    feat(ui-service): add CSS styling for home, navbar, fotter and cards

--- ui-service/src/main/java/com/enzo/uiservice/contoller/HomeController.java
+++ ui-service/src/main/java/com/enzo/uiservice/contoller/HomeController.java
@@ -1,13 +1,22 @@
 package com.enzo.uiservice.contoller;
 
+import com.enzo.uiservice.proxy.ChapterFeignClient;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
+    private final ChapterFeignClient chapterFeignClient;
 
     @GetMapping("/")
-    public String home() {
+    public String home(HttpSession session, Model model) {
+        if (session.getAttribute("token") != null) {
+            model.addAttribute("chapters", chapterFeignClient.getAllChapter());
+        }
         return "home";
     }
 }
